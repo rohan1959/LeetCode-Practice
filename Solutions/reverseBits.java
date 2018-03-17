9dfa0307b89398d745eef4f744d02f0ecb30a8d2
Rohan <rbongur1@binghamton.edu>
2018-03-17 13:39:20 -0400

    added reverse bits solution

---
+++ Solutions/reverseBits.java
@@ -0,0 +1,20 @@
+Problem :
+    Reverse bits of a given 32 bits unsigned integer.
+
+    For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+    Follow up:
+    If this function is called many times, how would you optimize it?
+
+    Related problem: Reverse Integer
+
+Solution :
+
+public class Solution {
+    // you need treat n as an unsigned value
+  
+    public int reverseBits(int n) {
+        String binary = String.format("%32s",Integer.toBinaryString(n)).replace(' ', '0');
+        return (int) Integer.parseUnsignedInt(new StringBuilder(binary).reverse().toString(),2);
+    }
+}
